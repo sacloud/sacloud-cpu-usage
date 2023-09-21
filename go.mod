faf0520b113a1d26a99fc247ccc07ff872334645
Kazumichi Yamamoto <yamamoto.febc@gmail.com>
2023-09-21 17:48:54 +0900

    refactor: sacloud-usage-libの導入

--- go.mod
+++ go.mod
@@ -1,27 +1,32 @@
 module github.com/kazeburo/sacloud-cpu-usage
 
-go 1.18
+go 1.21
 
 require (
-	github.com/itchyny/gojq v0.12.7
-	github.com/jessevdk/go-flags v1.5.0
-	github.com/joho/godotenv v1.4.0
-	github.com/sacloud/iaas-api-go v1.3.2
+	github.com/sacloud/iaas-api-go v1.11.1
+	github.com/sacloud/sacloud-usage-lib v0.0.3
+	github.com/stretchr/testify v1.8.4
 )
 
 require (
 	github.com/andres-erbsen/clock v0.0.0-20160526145045-9e14626cd129 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/structs v1.1.0 // indirect
-	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
-	github.com/hashicorp/go-retryablehttp v0.7.1 // indirect
-	github.com/itchyny/timefmt-go v0.1.3 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.2 // indirect
+	github.com/itchyny/gojq v0.12.13 // indirect
+	github.com/itchyny/timefmt-go v0.1.5 // indirect
+	github.com/jessevdk/go-flags v1.5.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/sacloud/api-client-go v0.2.1 // indirect
-	github.com/sacloud/go-http v0.1.2 // indirect
-	github.com/sacloud/packages-go v0.0.5 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/sacloud/api-client-go v0.2.8 // indirect
+	github.com/sacloud/go-http v0.1.6 // indirect
+	github.com/sacloud/packages-go v0.0.9 // indirect
 	go.uber.org/ratelimit v0.2.0 // indirect
-	golang.org/x/crypto v0.0.0-20220321153916-2c7772ba3064 // indirect
-	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
+	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ main_test.go
@@ -0,0 +1,407 @@
+package main
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/sacloud/iaas-api-go"
+	"github.com/sacloud/iaas-api-go/types"
+	usage "github.com/sacloud/sacloud-usage-lib"
+	"github.com/stretchr/testify/require"
+)
+
+type stubIaasServerAPI struct {
+	servers  map[string][]*iaas.Server
+	activity map[string]map[string]*iaas.CPUTimeActivity // map[zone][resource-id]
+}
+
+var _ iaasServerAPI = (*stubIaasServerAPI)(nil)
+
+func (s *stubIaasServerAPI) Find(_ context.Context, zone string, _ *iaas.FindCondition) (*iaas.ServerFindResult, error) {
+	if _, ok := s.servers[zone]; !ok {
+		return &iaas.ServerFindResult{}, nil
+	}
+	return &iaas.ServerFindResult{
+		Total:   len(s.servers[zone]),
+		From:    0,
+		Count:   0,
+		Servers: s.servers[zone],
+	}, nil
+}
+
+func (s *stubIaasServerAPI) MonitorCPU(_ context.Context, zone string, id types.ID, _ *iaas.MonitorCondition) (*iaas.CPUTimeActivity, error) {
+	if _, ok := s.activity[zone]; !ok {
+		return &iaas.CPUTimeActivity{}, nil
+	}
+	if _, ok := s.activity[zone][id.String()]; !ok {
+		return &iaas.CPUTimeActivity{}, nil
+	}
+
+	return s.activity[zone][id.String()], nil
+}
+
+func Test_fetchResources(t *testing.T) {
+	type args struct {
+		client iaasServerAPI
+		opts   *usage.Option
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *usage.Resources
+		wantErr bool
+	}{
+		{
+			name: "empty",
+			args: args{
+				client: &stubIaasServerAPI{
+					servers:  map[string][]*iaas.Server{},
+					activity: nil,
+				},
+				opts: &usage.Option{},
+			},
+			want: &usage.Resources{
+				Resources: nil,
+				Label:     "servers",
+			},
+			wantErr: false,
+		},
+		{
+			name: "single resource - single value",
+			args: args{
+				client: &stubIaasServerAPI{
+					servers: map[string][]*iaas.Server{
+						"is1a": {
+							{
+								ID:   types.ID(1),
+								Name: "test1",
+								CPU:  1,
+							},
+						},
+					},
+					activity: map[string]map[string]*iaas.CPUTimeActivity{
+						"is1a": {
+							"1": {
+								Values: []*iaas.MonitorCPUTimeValue{
+									{
+										Time:    time.Unix(1, 0),
+										CPUTime: 1,
+									},
+								},
+							},
+						},
+					},
+				},
+				opts: &usage.Option{
+					Prefix: []string{"test"},
+					Zones:  []string{"is1a"},
+					Time:   1,
+				},
+			},
+			want: &usage.Resources{
+				Resources: []*usage.Resource{
+					{
+						ID:   1,
+						Name: "test1",
+						Zone: "is1a",
+						Monitors: []usage.MonitorValue{
+							{
+								Time:  time.Unix(1, 0),
+								Value: 1,
+							},
+						},
+						Label: "cpu_time",
+						AdditionalInfo: map[string]interface{}{
+							"cores": 1,
+						},
+					},
+				},
+				Label: "servers",
+			},
+		},
+		{
+			name: "single resource - multi values",
+			args: args{
+				client: &stubIaasServerAPI{
+					servers: map[string][]*iaas.Server{
+						"is1a": {
+							{
+								ID:   types.ID(1),
+								Name: "test1",
+								CPU:  1,
+							},
+						},
+					},
+					activity: map[string]map[string]*iaas.CPUTimeActivity{
+						"is1a": {
+							"1": {
+								Values: []*iaas.MonitorCPUTimeValue{
+									{
+										Time:    time.Unix(1, 0),
+										CPUTime: 1,
+									},
+									{
+										Time:    time.Unix(2, 0),
+										CPUTime: 2,
+									},
+									{
+										Time:    time.Unix(3, 0),
+										CPUTime: 3,
+									},
+								},
+							},
+						},
+					},
+				},
+				opts: &usage.Option{
+					Prefix: []string{"test"},
+					Zones:  []string{"is1a"},
+					Time:   3,
+				},
+			},
+			want: &usage.Resources{
+				Resources: []*usage.Resource{
+					{
+						ID:   1,
+						Name: "test1",
+						Zone: "is1a",
+						Monitors: []usage.MonitorValue{
+							{
+								Time:  time.Unix(1, 0),
+								Value: 1,
+							},
+							{
+								Time:  time.Unix(2, 0),
+								Value: 2,
+							},
+							{
+								Time:  time.Unix(3, 0),
+								Value: 3,
+							},
+						},
+						Label: "cpu_time",
+						AdditionalInfo: map[string]interface{}{
+							"cores": 1,
+						},
+					},
+				},
+				Label: "servers",
+			},
+		},
+		{
+			name: "multi resources - single value",
+			args: args{
+				client: &stubIaasServerAPI{
+					servers: map[string][]*iaas.Server{
+						"is1a": {
+							{
+								ID:   types.ID(1),
+								Name: "test1",
+								CPU:  1,
+							},
+						},
+						"is1b": {
+							{
+								ID:   types.ID(2),
+								Name: "test2",
+								CPU:  2,
+							},
+						},
+					},
+					activity: map[string]map[string]*iaas.CPUTimeActivity{
+						"is1a": {
+							"1": {
+								Values: []*iaas.MonitorCPUTimeValue{
+									{
+										Time:    time.Unix(1, 0),
+										CPUTime: 1,
+									},
+								},
+							},
+						},
+						"is1b": {
+							"2": {
+								Values: []*iaas.MonitorCPUTimeValue{
+									{
+										Time:    time.Unix(2, 0),
+										CPUTime: 2,
+									},
+								},
+							},
+						},
+					},
+				},
+				opts: &usage.Option{
+					Prefix: []string{"test"},
+					Zones:  []string{"is1a", "is1b"},
+					Time:   1,
+				},
+			},
+			want: &usage.Resources{
+				Resources: []*usage.Resource{
+					{
+						ID:   1,
+						Name: "test1",
+						Zone: "is1a",
+						Monitors: []usage.MonitorValue{
+							{
+								Time:  time.Unix(1, 0),
+								Value: 1,
+							},
+						},
+						Label: "cpu_time",
+						AdditionalInfo: map[string]interface{}{
+							"cores": 1,
+						},
+					},
+					{
+						ID:   2,
+						Name: "test2",
+						Zone: "is1b",
+						Monitors: []usage.MonitorValue{
+							{
+								Time:  time.Unix(2, 0),
+								Value: 2,
+							},
+						},
+						Label: "cpu_time",
+						AdditionalInfo: map[string]interface{}{
+							"cores": 2,
+						},
+					},
+				},
+				Label: "servers",
+			},
+		},
+		{
+			name: "multi resources - multi values",
+			args: args{
+				client: &stubIaasServerAPI{
+					servers: map[string][]*iaas.Server{
+						"is1a": {
+							{
+								ID:   types.ID(1),
+								Name: "test1",
+								CPU:  1,
+							},
+						},
+						"is1b": {
+							{
+								ID:   types.ID(2),
+								Name: "test2",
+								CPU:  2,
+							},
+						},
+					},
+					activity: map[string]map[string]*iaas.CPUTimeActivity{
+						"is1a": {
+							"1": {
+								Values: []*iaas.MonitorCPUTimeValue{
+									{
+										Time:    time.Unix(1, 0),
+										CPUTime: 1,
+									},
+									{
+										Time:    time.Unix(2, 0),
+										CPUTime: 2,
+									},
+									{
+										Time:    time.Unix(3, 0),
+										CPUTime: 3,
+									},
+								},
+							},
+						},
+						"is1b": {
+							"2": {
+								Values: []*iaas.MonitorCPUTimeValue{
+									{
+										Time:    time.Unix(4, 0),
+										CPUTime: 4,
+									},
+									{
+										Time:    time.Unix(5, 0),
+										CPUTime: 5,
+									},
+									{
+										Time:    time.Unix(6, 0),
+										CPUTime: 6,
+									},
+								},
+							},
+						},
+					},
+				},
+				opts: &usage.Option{
+					Prefix: []string{"test"},
+					Zones:  []string{"is1a", "is1b"},
+					Time:   3,
+				},
+			},
+			want: &usage.Resources{
+				Resources: []*usage.Resource{
+					{
+						ID:   1,
+						Name: "test1",
+						Zone: "is1a",
+						Monitors: []usage.MonitorValue{
+							{
+								Time:  time.Unix(1, 0),
+								Value: 1,
+							},
+							{
+								Time:  time.Unix(2, 0),
+								Value: 2,
+							},
+							{
+								Time:  time.Unix(3, 0),
+								Value: 3,
+							},
+						},
+						Label: "cpu_time",
+						AdditionalInfo: map[string]interface{}{
+							"cores": 1,
+						},
+					},
+					{
+						ID:   2,
+						Name: "test2",
+						Zone: "is1b",
+						Monitors: []usage.MonitorValue{
+							{
+								Time:  time.Unix(4, 0),
+								Value: 4,
+							},
+							{
+								Time:  time.Unix(5, 0),
+								Value: 5,
+							},
+							{
+								Time:  time.Unix(6, 0),
+								Value: 6,
+							},
+						},
+						Label: "cpu_time",
+						AdditionalInfo: map[string]interface{}{
+							"cores": 2,
+						},
+					},
+				},
+				Label: "servers",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := fetchResources(tt.args.client, tt.args.opts)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("fetchResources() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			got.Option = nil
+			tt.want.Option = nil
+			require.EqualValues(t, tt.want, got)
+		})
+	}
+}
